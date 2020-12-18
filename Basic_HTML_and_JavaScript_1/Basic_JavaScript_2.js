7cb20bea16a513863d9de6c2aec9b4e11a05dc57
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2020-12-18 18:53:28 -0500

    Add functions

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -1,6 +1,6 @@
+alert("This is a JavaScript alert box")
 function My_First_Function() {
 	var String = "Kiss me, I'm Irish";
 	var result = String.fontcolor("green");
-	document.getElementById("Irish").innerHTML = 
-	result;
+	document.GetElementById("Irish").innerHTML = result;
 }
